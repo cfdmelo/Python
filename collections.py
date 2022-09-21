e0e557574b1292e4aaf9e3a0bbb1d349b9e04b14
Camila Melo <107707007+cfdmelo@users.noreply.github.com>
2022-09-20 21:57:37 -0300

    Create collections.py

---
+++ collections.py
@@ -0,0 +1,46 @@
+# Criação de lista
+
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+# Acessando a lista
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+# alterar um item da lista
+
+myFruitList[2] = "orange" 
+print(myFruitList)
+
+# Criação de tuplas (imutáveis)
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+# Acessando as tuplas
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+# Dicionário
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+# Acessando dicionário
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
